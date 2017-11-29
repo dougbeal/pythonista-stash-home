5664f0d8b0dfef6ebf00004d1c9de1321b54073d
Douglas Beal <git@dougbeal.com>
2017-11-29 13:22:52 -0800

    ssh-keygen should use keyfile_prefix as filename

--- init-key.sh
+++ init-key.sh
@@ -1,19 +1,22 @@
 echo "ENV settings ${REMOTE_USERNAME} ${REMOTE_TARGET} ${MOBILE}"
+
 keyfile_prefix="stash-pythonista-${MOBILE}"
 echo ${keyfile_prefix}
+keyfile_glob="stash-pythonista*"
+echo ${keyfile_glob}
+
 sshdir="${STASH_ROOT}/.ssh"
 echo ${sshdir}
 tempdir="${STASH_ROOT}/temp"
 echo ${tempdir}
-keyfile_glob="stash-pythonista*"
-echo ${keyfile_glob}
-echo Settings ${sshdir) ${keyfile_glob} sshdir ${sshdir} tempdir ${tempdir}
 
-mkdir -p "${sshdir}"
+echo "Settings ${keyfile_prefix) ${keyfile_glob} sshdir ${sshdir} tempdir ${tempdir}"
+
+mkdir -p "{sshdir}"
 # scp cannot find key so it will prompt user for password
 mkdir -p "${tempdir}"
 cd "${tempdir}"
-ssh-keygen -t dsa -f ${sshdir}
+ssh-keygen -t dsa -f ${keyfile_prefix}
 scp ${keyfile_glob} ${REMOTE_USERNAME}@${REMOTE_TARGET}:~/.ssh/
 cp ${keyfile_glob} "${sshdir}"
 
