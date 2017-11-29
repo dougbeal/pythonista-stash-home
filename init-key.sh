558681cf872def93957f4aceb0e7db35fe959b4b
Douglas Beal <git@dougbeal.com>
2017-11-29 13:11:54 -0800

    more echos, something is wrong with env variables

--- init-key.sh
+++ init-key.sh
@@ -1,12 +1,14 @@
 echo "ENV settings ${REMOTE_USERNAME} ${REMOTE_TARGET} ${MOBILE}"
 SSH_FILENAME="stash-pythonista-${MOBILE}"
+echo ${SSH_FILENAME}
 sshdir="${STASH_ROOT}/.ssh"
+echo ${sshdir}
 tempdir="${STASH_ROOT}/temp"
 files="stash-pythonista*"
 echo "Settings ${SSH_FILENAME) ${files} sshdir ${sshdir} tempdir ${tempdir}"
 
 mkdir -p "${sshdir}"
-# scp can't find key so it will prompt user for password
+# scp cannot find key so it will prompt user for password
 mkdir -p "${tempdir}"
 cd "${tempdir}"
 ssh-keygen -t dsa -f ${SSH_FILENAME}
