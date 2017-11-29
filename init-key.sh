b194864933037ef24352d60cded3b5174754a5aa
Douglas Beal <git@dougbeal.com>
2017-11-29 13:18:51 -0800

    Fixed duplicated variables

--- init-key.sh
+++ init-key.sh
@@ -1,20 +1,20 @@
 echo "ENV settings ${REMOTE_USERNAME} ${REMOTE_TARGET} ${MOBILE}"
-sshdir="stash-pythonista-${MOBILE}"
-echo ${sshdir}
+keyfile_prefix="stash-pythonista-${MOBILE}"
+echo ${keyfile_prefix}
 sshdir="${STASH_ROOT}/.ssh"
 echo ${sshdir}
 tempdir="${STASH_ROOT}/temp"
 echo ${tempdir}
-keyfiles="stash-pythonista*"
-echo ${keyfiles}
-echo Settings ${sshdir) ${keyfiles} sshdir ${sshdir} tempdir ${tempdir}
+keyfile_glob="stash-pythonista*"
+echo ${keyfile_glob}
+echo Settings ${sshdir) ${keyfile_glob} sshdir ${sshdir} tempdir ${tempdir}
 
 mkdir -p "${sshdir}"
 # scp cannot find key so it will prompt user for password
 mkdir -p "${tempdir}"
 cd "${tempdir}"
 ssh-keygen -t dsa -f ${sshdir}
-scp ${keyfiles} ${REMOTE_USERNAME}@${REMOTE_TARGET}:~/.ssh/
-cp ${keyfiles} "${sshdir}"
+scp ${keyfile_glob} ${REMOTE_USERNAME}@${REMOTE_TARGET}:~/.ssh/
+cp ${keyfile_glob} "${sshdir}"
 
 
