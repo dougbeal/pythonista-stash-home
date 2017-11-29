24b338dac92b27e616ce4a654f7feade3511a25b
Douglas Beal <git@dougbeal.com>
2017-11-29 13:15:07 -0800

    renamed local variables to lowercase

--- init-key.sh
+++ init-key.sh
@@ -1,19 +1,19 @@
 echo "ENV settings ${REMOTE_USERNAME} ${REMOTE_TARGET} ${MOBILE}"
-SSH_FILENAME="stash-pythonista-${MOBILE}"
-echo ${SSH_FILENAME}
+sshdir="stash-pythonista-${MOBILE}"
+echo ${sshdir}
 sshdir="${STASH_ROOT}/.ssh"
 echo ${sshdir}
 tempdir="${STASH_ROOT}/temp"
 echo ${tempdir}
-files="stash-pythonista*"
-echo "Settings ${SSH_FILENAME) ${files} sshdir ${sshdir} tempdir ${tempdir}"
+keyfiles="stash-pythonista*"
+echo "Settings ${sshdir) ${keyfiles} sshdir ${sshdir} tempdir ${tempdir}"
 
 mkdir -p "${sshdir}"
 # scp cannot find key so it will prompt user for password
 mkdir -p "${tempdir}"
 cd "${tempdir}"
-ssh-keygen -t dsa -f ${SSH_FILENAME}
-scp ${files} ${REMOTE_USERNAME}@${REMOTE_TARGET}:~/.ssh/
-cp ${files} "${sshdir}"
+ssh-keygen -t dsa -f ${sshdir}
+scp ${keyfiles} ${REMOTE_USERNAME}@${REMOTE_TARGET}:~/.ssh/
+cp ${keyfiles} "${sshdir}"
 
 
