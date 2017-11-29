be895e440fe6c0072ac0021e7d0c2954522768a8
Douglas Beal <git@dougbeal.com>
2017-11-29 13:01:05 -0800

    echo env settings removed --password from scp

--- init-key.sh
+++ init-key.sh
@@ -1,15 +1,16 @@
 
 SSH_FILENAME="stash-pythonista-${MOBILE}"
-echo "Need remote password for ${REMOTE_USERNAME} on ${REMOTE_TARGET}"
-read REMOTE_PASSWORD
 sshdir="$STASH_ROOT/.ssh"
 tempdir="$STASH_ROOT/temp"
 files="stash-pythonista*"
-mkdir -p "$sshdir"
+echo "Settings ${SSH_FILENAME) ${files} sshdir ${sshdir} tempdir ${tempdir}"
+
+mkdir -p "${sshdir}"
 # scp can't find key so it will prompt user for password
-mkdir -p "$tempdr"
-cd "$tempdr"
+mkdir -p "${tempdir}"
+cd "${tempdir}"
 ssh-keygen -t dsa -f ${SSH_FILENAME}
-scp --password ${REMOTE_PASSWORD} ${files} ${REMOTE_USERNAME}@${REMOTE_TARGET}:~/.ssh/
-cp ${files} "$sshdir"
+scp ${files} ${REMOTE_USERNAME}@${REMOTE_TARGET}:~/.ssh/
+cp ${files} "${sshdir}"
+
 
