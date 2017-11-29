92b1f63c850a33ff3bc4e940f2e97322157de6e3
Douglas Beal <git@dougbeal.com>
2017-11-29 12:51:01 -0800

    create ssh key in temp directory so scp will prompt for password

--- init-key.sh
+++ init-key.sh
@@ -2,8 +2,14 @@
 SSH_FILENAME="stash-pythonista-${MOBILE}"
 echo "Need remote password for ${REMOTE_USERNAME} on ${REMOTE_TARGET}"
 read REMOTE_PASSWORD
-mkdir -p "$STASH_ROOT/.ssh"
-cd "$STASH_ROOT/.ssh"
+sshdir="$STASH_ROOT/.ssh"
+tempdir="$STASH_ROOT/temp"
+files="stash-pythonista*"
+mkdir -p "$sshdir"
+# scp can't find key so it will prompt user for password
+mkdir -p "$tempdr"
+cd "$tempdr"
 ssh-keygen -t dsa -f ${SSH_FILENAME}
-scp --password ${REMOTE_PASSWORD} stash-pythonista* ${REMOTE_USERNAME}@${REMOTE_TARGET}:~/.ssh/
+scp --password ${REMOTE_PASSWORD} ${files} ${REMOTE_USERNAME}@${REMOTE_TARGET}:~/.ssh/
+cp ${files} "$sshdir"
 
