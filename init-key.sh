6bd67dfd035bd68d99b80b7fc64284fdc319593d
Douglas Beal <git@dougbeal.com>
2017-11-29 13:24:04 -0800

    split debug echo into 2 lines

--- init-key.sh
+++ init-key.sh
@@ -10,7 +10,8 @@ echo ${sshdir}
 tempdir="${STASH_ROOT}/temp"
 echo ${tempdir}
 
-echo "Settings ${keyfile_prefix) ${keyfile_glob} sshdir ${sshdir} tempdir ${tempdir}"
+echo "Settings ${keyfile_prefix) ${keyfile_glob}"
+echo "Directories sshdir ${sshdir} tempdir ${tempdir}"
 
 mkdir -p "{sshdir}"
 # scp cannot find key so it will prompt user for password
