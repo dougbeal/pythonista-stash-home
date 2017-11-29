7d09744169446f58d1d5700a12d8b12c339f55df
Douglas Beal <git@dougbeal.com>
2017-11-29 13:28:06 -0800

    ls generated keyfiles, echo only likes a single variable,

--- init-key.sh
+++ init-key.sh
@@ -1,25 +1,21 @@
 echo "ENV settings ${REMOTE_USERNAME} ${REMOTE_TARGET} ${MOBILE}"
 
 keyfile_prefix="stash-pythonista-${MOBILE}"
-echo ${keyfile_prefix}
+echo "keyfile_prefix ${keyfile_prefix}"
 keyfile_glob="stash-pythonista*"
 echo ${keyfile_glob}
-
-#echo "Settings ${keyfile_prefix) ${keyfile_glob}"
 
 sshdir="${STASH_ROOT}/.ssh"
 echo ${sshdir}
 tempdir="${STASH_ROOT}/temp"
 echo ${tempdir}
 
-#echo "Settings ${keyfile_prefix) ${keyfile_glob}"
-#echo "Directories sshdir ${sshdir} tempdir ${tempdir}"
-
 mkdir -p "{sshdir}"
 # scp cannot find key so it will prompt user for password
 mkdir -p "${tempdir}"
 cd "${tempdir}"
 ssh-keygen -t dsa -f ${keyfile_prefix}
+ls ${keyfile_glob}
 scp ${keyfile_glob} ${REMOTE_USERNAME}@${REMOTE_TARGET}:~/.ssh/
 cp ${keyfile_glob} "${sshdir}"
 
